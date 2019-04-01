a0b6cf2e585594f8addf48db8c9745adc97a43d2
Pavel Filatov <p.filatov@smartweb.com.ua>
2019-04-01 15:15:06 +0300

    changed logic of collector

--- prometheus_client/process_collector.py
+++ prometheus_client/process_collector.py
@@ -52,43 +52,60 @@ class ProcessCollector(object):
         if not self._btime:
             return []
         result = []
+        vmem_value = 0
+        rss_value = 0
+        parts = None
+
         for p in self._pid:
             pid = os.path.join(self._proc, str(p))
             try:
                 with open(os.path.join(pid, 'stat'), 'rb') as stat:
                     parts = (stat.read().split(b')')[-1].split())
 
-                vmem = GaugeMetricFamily(self._prefix + 'virtual_memory_bytes',
-                                         'Virtual memory size in bytes.', value=float(parts[20]))
-                rss = GaugeMetricFamily(self._prefix + 'resident_memory_bytes', 'Resident memory size in bytes.',
-                                        value=float(parts[21]) * _PAGESIZE)
-                start_time_secs = float(parts[19]) / self._ticks
-                start_time = GaugeMetricFamily(self._prefix + 'start_time_seconds',
-                                               'Start time of the process since unix epoch in seconds.',
-                                               value=start_time_secs + self._btime)
-                utime = float(parts[11]) / self._ticks
-                stime = float(parts[12]) / self._ticks
-                cpu = CounterMetricFamily(self._prefix + 'cpu_seconds_total',
-                                          'Total user and system CPU time spent in seconds.',
-                                          value=utime + stime)
-                result.extend([vmem, rss, start_time, cpu])
+                vmem_value += float(parts[20])
+
+                rss_value = float(parts[21]) * _PAGESIZE
             except IOError:
                 pass
 
-            try:
-                with open(os.path.join(pid, 'limits'), 'rb') as limits:
-                    for line in limits:
-                        if line.startswith(b'Max open file'):
-                            max_fds = GaugeMetricFamily(self._prefix + 'max_fds',
-                                                        'Maximum number of open file descriptors.',
-                                                        value=float(line.split()[3]))
-                            break
-                open_fds = GaugeMetricFamily(self._prefix + 'open_fds',
-                                             'Number of open file descriptors.',
-                                             len(os.listdir(os.path.join(pid, 'fd'))))
-                result.extend([open_fds, max_fds])
-            except (IOError, OSError):
-                pass
+        vmem = GaugeMetricFamily(self._prefix + 'virtual_memory_bytes',
+                                 'Virtual memory size in bytes.', value=vmem_value)
+        rss = GaugeMetricFamily(self._prefix + 'resident_memory_bytes',
+                                'Resident memory size in bytes.',
+                                value=rss_value)
+
+        start_time = GaugeMetricFamily(self._prefix + 'start_time_seconds',
+                                           'Start time of the process since unix epoch in seconds.',
+                                           value=0)
+        cpu = CounterMetricFamily(self._prefix + 'cpu_seconds_total',
+                                      'Total user and system CPU time spent in seconds.',
+                                      value=0)
+        if parts:
+            start_time_secs = float(parts[19]) / self._ticks
+            start_time = GaugeMetricFamily(self._prefix + 'start_time_seconds',
+                                           'Start time of the process since unix epoch in seconds.',
+                                           value=start_time_secs + self._btime)
+            utime = float(parts[11]) / self._ticks
+            stime = float(parts[12]) / self._ticks
+            cpu = CounterMetricFamily(self._prefix + 'cpu_seconds_total',
+                                      'Total user and system CPU time spent in seconds.',
+                                      value=utime + stime)
+        result.extend([vmem, rss, start_time, cpu])
+
+        # try:
+        #     with open(os.path.join(pid, 'limits'), 'rb') as limits:
+        #         for line in limits:
+        #             if line.startswith(b'Max open file'):
+        max_fds = GaugeMetricFamily(self._prefix + 'max_fds',
+                                    'Maximum number of open file descriptors.',
+                                    value=0)
+        #                 break
+        open_fds = GaugeMetricFamily(self._prefix + 'open_fds',
+                                     'Number of open file descriptors.',
+                                     0)
+        result.extend([open_fds, max_fds])
+        # except (IOError, OSError):
+        #     pass
 
         return result
 
